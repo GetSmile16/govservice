f48d42f1333cde422de316a814f8f421d1aafa7b
GetSmile16 <ivlad160101@gmail.com>
2023-11-15 16:06:32 +0700

    [k8s] Add actuator

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,6 +1,7 @@
 INSERT INTO users (id, email, first_name, last_name, patronymic, password)
 VALUES (1, 'example@gmail.com', 'admin', 'admin', 'admin',
         '$2a$10$zoKxnNqBBWb9Vm9hk5qTieKc4gCIdBC.ukAtrMqooVZlRWk6gXhfC');
+-- password - admin
 
 SELECT setval('users_id_seq', 1);
 
